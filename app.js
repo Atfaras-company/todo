839fb12272bcf39b399b0f30f22fd2cd5268835f
Xezretov Mergen <mxezretov@gmail.com>
2023-11-07 17:11:55 +0500

    All design is finish

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const ul = document.querySelector('ul');
-const input = document.getElementById('inputTodo');
+const input = document.getElementById('item');
 let itemsArray = localStorage.getItem('items') ?
 JSON.parse(localStorage.getItem('items')) : [];
 
 itemsArray.forEach(addTask);
 function addTask(text){
-  const li = document.createElement('li')
-  li.textContent = text;
+  const li = document.createElement('li');
+  li.innerHTML = text + '<div id="icons"> <i id="edit" class="fa-solid fa-pen-to-square"></i><i id="delete" class="fa-solid fa-trash"></i><i class="fa-solid fa-check"></i></div> ';
   ul.appendChild(li);
 }
 
-function addTodo(){
-  itemsArray.push(inputTodo.value);
+function add(){
+  itemsArray.push(input.value);
   localStorage.setItem('items', JSON.stringify(itemsArray));
-  addTask(inputTodo.value);
-  inputTodo.value = '';
+  addTask(input.value);
+  input.value = '';
 } 
 
 function del(){
   localStorage.clear();
   ul.innerHTML = '';
   itemsArray = [];
-}``  
+}
